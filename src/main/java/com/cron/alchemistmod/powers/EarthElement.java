a6ed93a33ece301d27f16e8a4d98548189afc962
IgpayAtenlay <mscron7@gmail.com>
2024-11-08 17:24:46 -0500

    added earth element

---
+++ src/main/java/com/cron/alchemistmod/powers/EarthElement.java
@@ -0,0 +1,94 @@
+package com.cron.alchemistmod.powers;
+
+import basemod.interfaces.CloneablePowerInterface;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.cron.alchemistmod.AlchemistMod;
+import com.cron.alchemistmod.util.TextureLoader;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.RemoveSpecificPowerAction;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.localization.PowerStrings;
+import com.megacrit.cardcrawl.powers.AbstractPower;
+import com.megacrit.cardcrawl.powers.DexterityPower;
+
+public class EarthElement extends AbstractPower implements CloneablePowerInterface {
+    public AbstractCreature source;
+
+    public static final String POWER_ID = AlchemistMod.makeID(EarthElement.class.getSimpleName());
+    private static final PowerStrings powerStrings = CardCrawlGame.languagePack.getPowerStrings(POWER_ID);
+    public static final String NAME = powerStrings.NAME;
+    public static final String[] DESCRIPTIONS = powerStrings.DESCRIPTIONS;
+
+    // We create 2 new textures *Using This Specific Texture Loader* - an 84x84 image and a 32x32 one.
+    // There's a fallback "missing texture" image, so the game shouldn't crash if you accidentally put a non-existent file.
+    private static final Texture tex84 = TextureLoader.getTexture(AlchemistMod.makePowerPath("placeholder_power84.png"));
+    private static final Texture tex32 = TextureLoader.getTexture(AlchemistMod.makePowerPath("placeholder_power32.png"));
+
+    public EarthElement(final AbstractCreature owner, final AbstractCreature source, final int amount) {
+        name = NAME;
+        ID = POWER_ID;
+
+        this.owner = owner;
+        this.amount = amount;
+        this.source = source;
+
+        type = PowerType.BUFF;
+        isTurnBased = false;
+
+        // We load those txtures here.
+        this.region128 = new TextureAtlas.AtlasRegion(tex84, 0, 0, 84, 84);
+        this.region48 = new TextureAtlas.AtlasRegion(tex32, 0, 0, 32, 32);
+
+        updateDescription();
+    }
+
+    // Note: If you want to apply an effect when a power is being applied you have 3 options:
+    //onInitialApplication is "When THIS power is first applied for the very first time only."
+    //onApplyPower is "When the owner applies a power to something else (only used by Sadistic Nature)."
+    //onReceivePowerPower from StSlib is "When any (including this) power is applied to the owner."
+
+
+    @Override
+    public void onInitialApplication() {
+        super.onInitialApplication();
+        brewPotion();
+    }
+
+    @Override
+    public void stackPower(int stackAmount) {
+        this.fontScale = 8.0F;
+        this.amount += stackAmount;
+        brewPotion();
+    }
+
+    public void brewPotion() {
+        if (false) {
+            // if there is another element already
+            this.amount -= 1;
+        }
+
+        while (this.amount >= 2) {
+            AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(owner, owner,
+                    new DexterityPower(owner, 1), 1));
+            this.amount -= 2;
+        }
+
+        if (this.amount == 0) {
+            AbstractDungeon.actionManager.addToTop(new RemoveSpecificPowerAction(this.owner, this.owner, EarthElement.POWER_ID));
+        }
+    }
+
+    // Update the description when you apply this power. (i.e. add or remove an "s" in keyword(s))
+    @Override
+    public void updateDescription() {
+        description = DESCRIPTIONS[0];
+    }
+
+    @Override
+    public AbstractPower makeCopy() {
+        return new EarthElement(owner, source, amount);
+    }
+}
