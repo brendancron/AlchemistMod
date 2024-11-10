b718334ab26a72057cbe1a7cf29f4cc33e24fa6b
IgpayAtenlay <mscron7@gmail.com>
2024-11-10 16:20:05 -0500

    moved repeating functions from individual elements to ElementPower

--- src/main/java/com/cron/alchemistmod/powers/DarkElement.java
+++ src/main/java/com/cron/alchemistmod/powers/DarkElement.java
@@ -14,56 +14,22 @@ import com.megacrit.cardcrawl.potions.*;
 import com.megacrit.cardcrawl.powers.AbstractPower;
 
 public class DarkElement extends ElementPower {
-    public AbstractCreature source;
-
     public static final String POWER_ID = AlchemistMod.makeID(DarkElement.class.getSimpleName());
     private static final PowerStrings powerStrings = CardCrawlGame.languagePack.getPowerStrings(POWER_ID);
-    public static final String NAME = powerStrings.NAME;
-    public static final String[] DESCRIPTIONS = powerStrings.DESCRIPTIONS;
 
     private static final Texture tex84 = TextureLoader.getTexture(AlchemistMod.makePowerPath("placeholder_power84.png"));
     private static final Texture tex32 = TextureLoader.getTexture(AlchemistMod.makePowerPath("placeholder_power32.png"));
 
     public DarkElement(final AbstractCreature owner, final AbstractCreature source, final int amount) {
-        name = NAME;
-        ID = POWER_ID;
+        super(owner, source, amount);
 
-        this.owner = owner;
-        this.amount = amount;
-        this.source = source;
-
-        type = PowerType.BUFF;
-        isTurnBased = false;
+        this.name = powerStrings.NAME;
+        this.ID = POWER_ID;
 
         this.region128 = new TextureAtlas.AtlasRegion(tex84, 0, 0, 84, 84);
         this.region48 = new TextureAtlas.AtlasRegion(tex32, 0, 0, 32, 32);
 
         updateDescription();
-    }
-
-    @Override
-    public void onInitialApplication() {
-        super.onInitialApplication();
-        testForPotions();
-    }
-
-    @Override
-    public void stackPower(int stackAmount) {
-        this.fontScale = 8.0F;
-        this.amount += stackAmount;
-        testForPotions();
-    }
-
-    public void brewPotion(String elementID, AbstractPotion potion) {
-        AbstractDungeon.actionManager.addToBottom(new ObtainPotionAction(potion));
-        AbstractPower element = owner.getPower(elementID);
-
-        element.amount -= 1;
-        this.amount -= 1;
-
-        if (element.amount == 0) {
-            AbstractDungeon.actionManager.addToTop(new RemoveSpecificPowerAction(this.owner, this.owner, elementID));
-        }
     }
 
     public void testForPotions() {
@@ -96,7 +62,7 @@ public class DarkElement extends ElementPower {
 
     @Override
     public void updateDescription() {
-        description = DESCRIPTIONS[0];
+        this.description = powerStrings.DESCRIPTIONS[0];
     }
 
     @Override
