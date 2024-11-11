8bde57709a63700ee2b91f379ae3c94eb91ce3d0
Brendan Cron <bcron@umd.edu>
2024-11-10 19:56:36 -0500

    added transmute

---
+++ src/main/java/com/cron/alchemistmod/cards/Transmute.java
@@ -0,0 +1,50 @@
+package com.cron.alchemistmod.cards;
+
+import basemod.abstracts.CustomCard;
+import com.cron.alchemistmod.AlchemistMod;
+import com.cron.alchemistmod.actions.ChooseAndTransformRandomCardAction;
+import com.cron.alchemistmod.characters.TheAlchemist;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+public class Transmute extends CustomCard {
+    private static final CardRarity RARITY = CardRarity.COMMON;
+    private static final CardTarget TARGET = CardTarget.SELF;
+    private static final CardType TYPE = CardType.SKILL;
+    public static final CardColor COLOR = TheAlchemist.Enums.COLOR_GRAY;
+
+    private static final int COST = 1;
+    private static final int DAMAGE = 1;
+
+    public final static String ID = AlchemistMod.makeID(Transmute.class.getSimpleName());
+    public static final String NAME = CardCrawlGame.languagePack.getCardStrings(ID).NAME;
+    public static final String DESCRIPTION = CardCrawlGame.languagePack.getCardStrings(ID).DESCRIPTION;
+    public static final String IMG_PATH = AlchemistMod.makeCardPath(Transmute.class.getSimpleName() + ".png");
+
+    public Transmute() {
+        super(ID, NAME, IMG_PATH, COST, DESCRIPTION, TYPE, COLOR, RARITY, TARGET);
+        this.baseDamage = DAMAGE;
+        this.exhaust = true;
+    }
+
+    @Override
+    public AbstractCard makeCopy() {
+        return new Transmute();
+    }
+
+    @Override
+    public void upgrade() {
+        if (!this.upgraded) {
+            this.upgradeName();
+            this.exhaust = false;
+        }
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        AbstractDungeon.actionManager.addToBottom(new ChooseAndTransformRandomCardAction());
+    }
+}
