8fc30fa2e72ef9440dad81ef9c03f107c1a03e7b
IgpayAtenlay <mscron7@gmail.com>
2024-11-22 17:20:26 -0500

    added splash and wildfire

--- src/main/java/com/cron/alchemistmod/cards/Splash.java
+++ src/main/java/com/cron/alchemistmod/cards/Splash.java
@@ -1,15 +1,16 @@
 package com.cron.alchemistmod.cards;
 
 import com.cron.alchemistmod.AlchemistMod;
-import com.cron.alchemistmod.actions.ChooseAndTransformRandomCardAction;
 import com.cron.alchemistmod.characters.TheAlchemist;
-import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.cron.alchemistmod.powers.WaterElement;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.localization.CardStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 
-public class Transmute extends AbstractAlchemistCard {
+public class Splash extends AbstractAlchemistCard {
     private static final CardRarity RARITY = CardRarity.COMMON;
     private static final CardTarget TARGET = CardTarget.SELF;
     private static final CardType TYPE = CardType.SKILL;
@@ -18,19 +19,12 @@ public class Transmute extends AbstractAlchemistCard {
     private static final int COST = 1;
     private static final int COST_UPGRADE = 0;
 
-    public final static String ID = AlchemistMod.makeID(Transmute.class.getSimpleName());
-    public static final String NAME = CardCrawlGame.languagePack.getCardStrings(ID).NAME;
-    public static final String DESCRIPTION = CardCrawlGame.languagePack.getCardStrings(ID).DESCRIPTION;
-    public static final String IMG_PATH = AlchemistMod.makeCardPath(Transmute.class.getSimpleName() + ".png");
+    public final static String ID = AlchemistMod.makeID(Splash.class.getSimpleName());
+    public static final CardStrings CARD_STRINGS = CardCrawlGame.languagePack.getCardStrings(ID);
+    public static final String IMG_PATH = AlchemistMod.makeCardPath(Splash.class.getSimpleName() + ".png");
 
-    public Transmute() {
-        super(ID, NAME, IMG_PATH, COST, DESCRIPTION, TYPE, COLOR, RARITY, TARGET);
-        this.exhaust = true;
-    }
-
-    @Override
-    public AbstractCard makeCopy() {
-        return new Transmute();
+    public Splash() {
+        super(ID, CARD_STRINGS.NAME, IMG_PATH, COST, CARD_STRINGS.DESCRIPTION, TYPE, COLOR, RARITY, TARGET);
     }
 
     @Override
@@ -43,6 +37,8 @@ public class Transmute extends AbstractAlchemistCard {
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        AbstractDungeon.actionManager.addToBottom(new ChooseAndTransformRandomCardAction());
+        AbstractDungeon.actionManager.addToBottom(
+                new ApplyPowerAction(p, p, new WaterElement(p, p, 1), 1)
+        );
     }
 }
