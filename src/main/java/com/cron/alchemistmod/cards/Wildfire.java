917a51fc99d770baaa4e7b9d6bf87799bf5bb0f0
IgpayAtenlay <mscron7@gmail.com>
2024-11-22 17:20:26 -0500

    added splash and wildfire

--- src/main/java/com/cron/alchemistmod/cards/Wildfire.java
+++ src/main/java/com/cron/alchemistmod/cards/Wildfire.java
@@ -2,7 +2,9 @@ package com.cron.alchemistmod.cards;
 
 import com.cron.alchemistmod.AlchemistMod;
 import com.cron.alchemistmod.characters.TheAlchemist;
+import com.cron.alchemistmod.powers.FireElement;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.actions.common.DamageAllEnemiesAction;
 import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
@@ -11,32 +13,35 @@ import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.localization.CardStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 
-public class Prohibition extends AbstractAlchemistCard {
-    private static final CardRarity RARITY = CardRarity.RARE;
+public class Wildfire extends AbstractAlchemistCard {
+    private static final CardRarity RARITY = CardRarity.UNCOMMON;
     private static final CardTarget TARGET = CardTarget.ALL_ENEMY;
     private static final CardType TYPE = CardType.ATTACK;
     public static final CardColor COLOR = TheAlchemist.Enums.COLOR_GRAY;
 
     private static final int COST = 2;
-    private static final int DAMAGE = 26;
-    private static final int DAMAGE_UPGRADE = 8;
+    private static final int DAMAGE = 12;
     private static final boolean MULTI_DAMAGE = true;
 
-    public final static String ID = AlchemistMod.makeID(Prohibition.class.getSimpleName());
-    public static final CardStrings CARD_STRINGS = CardCrawlGame.languagePack.getCardStrings(ID);
-    public static final String IMG_PATH = AlchemistMod.makeCardPath(Prohibition.class.getSimpleName() + ".png");
 
-    public Prohibition() {
+    public final static String ID = AlchemistMod.makeID(Wildfire.class.getSimpleName());
+    public static final CardStrings CARD_STRINGS = CardCrawlGame.languagePack.getCardStrings(ID);
+    public static final String IMG_PATH = AlchemistMod.makeCardPath(Wildfire.class.getSimpleName() + ".png");
+
+    public Wildfire() {
         super(ID, CARD_STRINGS.NAME, IMG_PATH, COST, CARD_STRINGS.DESCRIPTION, TYPE, COLOR, RARITY, TARGET);
         this.baseDamage = DAMAGE;
         this.isMultiDamage = MULTI_DAMAGE;
+        this.exhaust = true;
     }
 
     @Override
     public void upgrade() {
         if (!this.upgraded) {
             this.upgradeName();
-            this.upgradeDamage(DAMAGE_UPGRADE);
+            this.exhaust = false;
+            this.rawDescription = CARD_STRINGS.UPGRADE_DESCRIPTION;
+            this.initializeDescription();
         }
     }
 
@@ -45,18 +50,12 @@ public class Prohibition extends AbstractAlchemistCard {
         AbstractDungeon.actionManager.addToBottom(
                 new DamageAllEnemiesAction(p, this.multiDamage, DamageInfo.DamageType.NORMAL, AbstractGameAction.AttackEffect.NONE)
         );
-    }
 
-    public boolean canUse(AbstractPlayer p, AbstractMonster m) {
-        boolean canUse = super.canUse(p, m);
-        if (!canUse) {
-            return false;
-        } else {
-            if (p.hasAnyPotions()) {
-                this.cantUseMessage = CARD_STRINGS.EXTENDED_DESCRIPTION[0];
-                return false;
-            } else {
-                return true;
+        for (AbstractMonster monster : AbstractDungeon.getCurrRoom().monsters.monsters) {
+            if (!monster.isDeadOrEscaped()) {
+                AbstractDungeon.actionManager.addToBottom(
+                        new ApplyPowerAction(p, p, new FireElement(p, p, 1), 1)
+                );
             }
         }
     }
