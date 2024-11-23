676bd8edca90f06276ac384222f9f0d664b1bb5f
IgpayAtenlay <mscron7@gmail.com>
2024-11-22 19:40:53 -0500

    added Recipes

---
+++ src/main/java/com/cron/alchemistmod/cards/Recipes.java
@@ -0,0 +1,74 @@
+package com.cron.alchemistmod.cards;
+
+import com.cron.alchemistmod.AlchemistMod;
+import com.cron.alchemistmod.characters.TheAlchemist;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.localization.CardStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+public class Recipes extends AbstractAlchemistCard {
+    private static final CardRarity RARITY = CardRarity.UNCOMMON;
+    private static final CardTarget TARGET = CardTarget.ENEMY;
+    private static final CardType TYPE = CardType.ATTACK;
+    public static final CardColor COLOR = TheAlchemist.Enums.COLOR_GRAY;
+
+    private static final int COST = 1;
+    private static final int DAMAGE = 0;
+    private static final int MAGIC = 2;
+    private static final int MAGIC_UPGRADE = 1;
+
+    public final static String ID = AlchemistMod.makeID(Recipes.class.getSimpleName());
+    public static final CardStrings CARD_STRINGS = CardCrawlGame.languagePack.getCardStrings(ID);
+    public static final String IMG_PATH = AlchemistMod.makeCardPath(Recipes.class.getSimpleName() + ".png");
+
+    public Recipes() {
+        super(ID, CARD_STRINGS.NAME, IMG_PATH, COST, CARD_STRINGS.DESCRIPTION, TYPE, COLOR, RARITY, TARGET);
+        this.baseDamage = DAMAGE;
+        this.magicNumber = this.baseMagicNumber = MAGIC;
+    }
+
+    @Override
+    public void upgrade() {
+        if (!this.upgraded) {
+            this.upgradeName();
+            this.upgradeMagicNumber(MAGIC_UPGRADE);
+            this.initializeDescription();
+        }
+    }
+
+    @Override
+    protected void upgradeMagicNumber(int amount) {
+        this.baseMagicNumber -= amount;
+        this.magicNumber = this.baseMagicNumber;
+        this.upgradedMagicNumber = true;
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        AbstractDungeon.actionManager.addToBottom(
+                new DamageAction(m, new DamageInfo(p, this.damage, DamageInfo.DamageType.NORMAL))
+        );
+    }
+
+    public void applyPowers() {
+        this.baseDamage = countCards();
+        super.applyPowers();
+        this.rawDescription = CARD_STRINGS.DESCRIPTION + CARD_STRINGS.EXTENDED_DESCRIPTION[0];
+        this.initializeDescription();
+    }
+    public void calculateCardDamage(AbstractMonster mo) {
+        super.calculateCardDamage(mo);
+        this.rawDescription = CARD_STRINGS.DESCRIPTION + CARD_STRINGS.EXTENDED_DESCRIPTION[0];
+        this.initializeDescription();
+    }
+
+    public int countCards() {
+        int numOfCards = AbstractDungeon.player.hand.group.size() + AbstractDungeon.player.drawPile.group.size() + AbstractDungeon.player.discardPile.group.size();
+
+        return numOfCards / this.magicNumber;
+    }
+}
