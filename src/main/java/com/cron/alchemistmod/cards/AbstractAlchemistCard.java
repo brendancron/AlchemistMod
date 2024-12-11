0d0a0f292915c52a1a188d419b9e98e0f83f54a2
IgpayAtenlay <mscron7@gmail.com>
2024-12-10 22:34:19 -0500

    added card streamer

--- src/main/java/com/cron/alchemistmod/cards/AbstractAlchemistCard.java
+++ src/main/java/com/cron/alchemistmod/cards/AbstractAlchemistCard.java
@@ -2,6 +2,7 @@ package com.cron.alchemistmod.cards;
 
 import basemod.abstracts.CustomCard;
 import com.cron.alchemistmod.powers.AbstractElement;
+import com.megacrit.cardcrawl.potions.AbstractPotion;
 
 public abstract class AbstractAlchemistCard extends CustomCard {
     public AbstractAlchemistCard(String id, String name, String img, int cost, String rawDescription, CardType type, CardColor color, CardRarity rarity, CardTarget target) {
@@ -13,5 +14,14 @@ public abstract class AbstractAlchemistCard extends CustomCard {
     }
 
     public void triggerOnGainElement(AbstractElement element) {
+    }
+
+    public void triggerOnObtainPotion(AbstractPotion potion) {
+    }
+
+    public void triggerOnDestroyPotion() {
+    }
+
+    public void triggerOnRemovePotion(AbstractPotion potion) {
     }
 }
