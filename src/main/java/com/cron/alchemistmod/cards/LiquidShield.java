7f786695861b69da15f77f7fea176d673f49851a
IgpayAtenlay <mscron7@gmail.com>
2024-12-15 17:20:46 -0500

    added Liquid Shield

---
+++ src/main/java/com/cron/alchemistmod/cards/LiquidShield.java
@@ -0,0 +1,49 @@
+package com.cron.alchemistmod.cards;
+
+import com.cron.alchemistmod.AlchemistMod;
+import com.cron.alchemistmod.actions.RemovePotionAction;
+import com.cron.alchemistmod.characters.TheAlchemist;
+import com.megacrit.cardcrawl.actions.common.GainBlockAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.localization.CardStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+public class LiquidShield extends AbstractAlchemistCard {
+    private static final CardRarity RARITY = CardRarity.COMMON;
+    private static final CardTarget TARGET = CardTarget.SELF;
+    private static final CardType TYPE = CardType.SKILL;
+    public static final CardColor COLOR = TheAlchemist.Enums.COLOR_GRAY;
+
+    private static final int COST = 0;
+    private static final int BLOCK = 12;
+    private static final int BLOCK_UPGRADE = 4;
+
+    public final static String ID = AlchemistMod.makeID(LiquidShield.class.getSimpleName());
+    public static final CardStrings CARD_STRINGS = CardCrawlGame.languagePack.getCardStrings(ID);
+    public static final String IMG_PATH = AlchemistMod.makeCardPath(LiquidShield.class.getSimpleName() + ".png");
+
+    public LiquidShield() {
+        super(ID, CARD_STRINGS.NAME, IMG_PATH, COST, CARD_STRINGS.DESCRIPTION, TYPE, COLOR, RARITY, TARGET);
+        this.baseBlock = BLOCK;
+    }
+
+    @Override
+    public void upgrade() {
+        if (!this.upgraded) {
+            this.upgradeName();
+            this.upgradeBlock(BLOCK_UPGRADE);
+        }
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        AbstractDungeon.actionManager.addToBottom(
+                new RemovePotionAction(p, false)
+        );
+        AbstractDungeon.actionManager.addToBottom(
+                new GainBlockAction(p, p, this.block)
+        );
+    }
+}
