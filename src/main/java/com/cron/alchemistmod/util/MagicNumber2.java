8677de53c8d80c0e5c619455324f43a77f4f75d9
IgpayAtenlay <mscron7@gmail.com>
2024-12-15 12:36:27 -0500

    added second magic number and card mutagen

---
+++ src/main/java/com/cron/alchemistmod/util/MagicNumber2.java
@@ -0,0 +1,33 @@
+package com.cron.alchemistmod.util;
+
+import basemod.abstracts.DynamicVariable;
+import com.cron.alchemistmod.cards.AbstractAlchemistCard;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+
+public class MagicNumber2 extends DynamicVariable {
+
+    @Override
+    public String key() {
+        return "thealchemist:M";
+    }
+
+    @Override
+    public boolean isModified(AbstractCard card) {
+        return ((AbstractAlchemistCard) card).isMagicNumberTwoModified;
+    }
+
+    @Override
+    public int value(AbstractCard card) {
+        return ((AbstractAlchemistCard) card).magicNumberTwo;
+    }
+
+    @Override
+    public int baseValue(AbstractCard card) {
+        return ((AbstractAlchemistCard) card).baseMagicNumberTwo;
+    }
+
+    @Override
+    public boolean upgraded(AbstractCard card) {
+        return ((AbstractAlchemistCard) card).isMagicNumberTwoUpgraded;
+    }
+}
