e3f538dcda8238d4611550c8affa9140ac867ba9
IgpayAtenlay <mscron7@gmail.com>
2025-01-01 22:22:00 -0500

    added embers and poison shiv

---
+++ src/main/java/com/cron/alchemistmod/patches/ShivPatch.java
@@ -0,0 +1,36 @@
+package com.cron.alchemistmod.patches;
+
+import com.cron.alchemistmod.util.CustomTags;
+import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.powers.AccuracyPower;
+
+import java.util.ArrayList;
+
+@SpirePatch(
+        clz = AccuracyPower.class,
+        method = "updateExistingShivs",
+        paramtypez = { }
+)
+public class ShivPatch {
+    public static void Postfix(AccuracyPower __instance) {
+        updateShivs(AbstractDungeon.player.hand.group, __instance);
+        updateShivs(AbstractDungeon.player.drawPile.group, __instance);
+        updateShivs(AbstractDungeon.player.discardPile.group, __instance);
+        updateShivs(AbstractDungeon.player.exhaustPile.group, __instance);
+    }
+
+    public static void updateShivs(ArrayList<AbstractCard> cards, AccuracyPower __instance) {
+        for (AbstractCard card : cards) {
+            if (card.hasTag(CustomTags.SHIV)) {
+                if (!card.upgraded) {
+                    card.baseDamage = 4 + __instance.amount;
+                } else {
+                    card.baseDamage = 6 + __instance.amount;
+                }
+            }
+        }
+    }
+}
+
