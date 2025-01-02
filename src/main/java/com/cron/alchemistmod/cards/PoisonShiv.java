6d736d0bfb66abb9720ea7ec468e920cb0c91ee7
IgpayAtenlay <mscron7@gmail.com>
2025-01-01 22:22:00 -0500

    added embers and poison shiv

---
+++ src/main/java/com/cron/alchemistmod/cards/PoisonShiv.java
@@ -0,0 +1,62 @@
+package com.cron.alchemistmod.cards;
+
+import com.cron.alchemistmod.AlchemistMod;
+import com.cron.alchemistmod.characters.TheAlchemist;
+import com.cron.alchemistmod.util.CustomTags;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.localization.CardStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.PoisonPower;
+
+public class PoisonShiv extends AbstractAlchemistCard {
+    private static final CardRarity RARITY = CardRarity.COMMON;
+    private static final CardTarget TARGET = CardTarget.ENEMY;
+    private static final CardType TYPE = CardType.ATTACK;
+    public static final CardColor COLOR = TheAlchemist.Enums.COLOR_GRAY;
+
+    private static final int COST = 0;
+    private static final int DAMAGE = 4;
+    private static final int DAMAGE_UPGRADE = 2;
+    private static final int MAGIC = 2;
+    private static final int MAGIC_UPGRADE = 1;
+
+    public final static String ID = AlchemistMod.makeID(PoisonShiv.class.getSimpleName());
+    public static final CardStrings CARD_STRINGS = CardCrawlGame.languagePack.getCardStrings(ID);
+    public static final String IMG_PATH = AlchemistMod.makeCardPath(PoisonShiv.class.getSimpleName() + ".png");
+
+    public PoisonShiv() {
+        super(ID, CARD_STRINGS.NAME, IMG_PATH, COST, CARD_STRINGS.DESCRIPTION, TYPE, COLOR, RARITY, TARGET);
+        if (AbstractDungeon.player != null && AbstractDungeon.player.hasPower("Accuracy")) {
+            this.baseDamage = DAMAGE + AbstractDungeon.player.getPower("Accuracy").amount;
+        } else {
+            this.baseDamage = DAMAGE;
+        }
+
+        this.magicNumber = this.baseMagicNumber = MAGIC;
+        this.tags.add(CustomTags.SHIV);
+    }
+
+    @Override
+    public void upgrade() {
+        if (!this.upgraded) {
+            this.upgradeName();
+            this.upgradeDamage(DAMAGE_UPGRADE);
+            this.upgradeMagicNumber(MAGIC_UPGRADE);
+        }
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        AbstractDungeon.actionManager.addToBottom(
+                new DamageAction(m, new DamageInfo(p, this.damage, DamageInfo.DamageType.NORMAL))
+        );
+        AbstractDungeon.actionManager.addToBottom(
+                new ApplyPowerAction(m, p, new PoisonPower(m, p, this.magicNumber), this.magicNumber)
+        );
+    }
+}
---
+++ src/main/java/com/cron/alchemistmod/util/CustomTags.java
@@ -0,0 +1,9 @@
+package com.cron.alchemistmod.util;
+
+import com.evacipated.cardcrawl.modthespire.lib.SpireEnum;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+
+public class CustomTags {
+    @SpireEnum
+    public static AbstractCard.CardTags SHIV;
+}
