106a5c2d9780094d430e71bf9dfdeaff386a44e8
IgpayAtenlay <mscron7@gmail.com>
2025-01-22 12:54:12 -0500

    added air burst and pained strike

--- src/main/java/com/cron/alchemistmod/cards/PainedStrike.java
+++ src/main/java/com/cron/alchemistmod/cards/PainedStrike.java
@@ -9,36 +9,33 @@ import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.localization.CardStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
-import com.megacrit.cardcrawl.potions.AbstractPotion;
-import com.megacrit.cardcrawl.potions.PotionSlot;
 
-public class BagOfBricks extends AbstractAlchemistCard {
+public class PainedStrike extends AbstractAlchemistCard {
     private static final CardRarity RARITY = CardRarity.COMMON;
     private static final CardTarget TARGET = CardTarget.ENEMY;
     private static final CardType TYPE = CardType.ATTACK;
     public static final CardColor COLOR = TheAlchemist.Enums.COLOR_GRAY;
 
-    private static final int COST = 1;
+    private static final int COST = 3;
+    private static final int COST_UPGRADE = 2;
     private static final int DAMAGE = 0;
-    private static final int MAGIC = 3;
-    private static final int MAGIC_UPGRADE = 2;
 
 
-    public final static String ID = AlchemistMod.makeID(BagOfBricks.class.getSimpleName());
+    public final static String ID = AlchemistMod.makeID(PainedStrike.class.getSimpleName());
     public static final CardStrings CARD_STRINGS = CardCrawlGame.languagePack.getCardStrings(ID);
-    public static final String IMG_PATH = AlchemistMod.makeCardPath(BagOfBricks.class.getSimpleName() + ".png");
+    public static final String IMG_PATH = AlchemistMod.makeCardPath(PainedStrike.class.getSimpleName() + ".png");
 
-    public BagOfBricks() {
+    public PainedStrike() {
         super(ID, CARD_STRINGS.NAME, IMG_PATH, COST, CARD_STRINGS.DESCRIPTION, TYPE, COLOR, RARITY, TARGET);
         this.baseDamage = DAMAGE;
-        this.magicNumber = this.baseMagicNumber = MAGIC;
+        this.exhaust = true;
     }
 
     @Override
     public void upgrade() {
         if (!this.upgraded) {
             this.upgradeName();
-            this.upgradeMagicNumber(MAGIC_UPGRADE);
+            this.upgradeBaseCost(COST_UPGRADE);
             this.initializeDescription();
         }
     }
@@ -53,7 +50,7 @@ public class BagOfBricks extends AbstractAlchemistCard {
     }
 
     public void applyPowers() {
-        this.baseDamage = numOfPotions(AbstractDungeon.player) * this.magicNumber;
+        this.baseDamage = AbstractDungeon.player.maxHealth - AbstractDungeon.player.currentHealth;
         super.applyPowers();
         this.rawDescription = CARD_STRINGS.DESCRIPTION + CARD_STRINGS.EXTENDED_DESCRIPTION[0];
         this.initializeDescription();
@@ -62,16 +59,5 @@ public class BagOfBricks extends AbstractAlchemistCard {
         super.calculateCardDamage(mo);
         this.rawDescription = CARD_STRINGS.DESCRIPTION + CARD_STRINGS.EXTENDED_DESCRIPTION[0];
         this.initializeDescription();
-    }
-
-    public static int numOfPotions(AbstractPlayer player) {
-        int numOfPotions = 0;
-        for(AbstractPotion potion: player.potions) {
-            if (!(potion instanceof PotionSlot)) {
-                numOfPotions++;
-            }
-        }
-
-        return numOfPotions;
     }
 }
