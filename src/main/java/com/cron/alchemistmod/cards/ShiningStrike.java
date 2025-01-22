f2f08d836049409d714bbc2b1f39f5fe456ff877
IgpayAtenlay <mscron7@gmail.com>
2025-01-22 14:20:39 -0500

    added shining strike and chug

---
+++ src/main/java/com/cron/alchemistmod/cards/ShiningStrike.java
@@ -0,0 +1,53 @@
+package com.cron.alchemistmod.cards;
+
+import com.cron.alchemistmod.AlchemistMod;
+import com.cron.alchemistmod.characters.TheAlchemist;
+import com.cron.alchemistmod.powers.FireElement;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.localization.CardStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+public class FireStrike extends AbstractAlchemistCard {
+    private static final CardRarity RARITY = CardRarity.COMMON;
+    private static final CardTarget TARGET = CardTarget.ENEMY;
+    private static final CardType TYPE = CardType.ATTACK;
+    public static final CardColor COLOR = TheAlchemist.Enums.COLOR_GRAY;
+
+    private static final int COST = 1;
+    private static final int DAMAGE = 6;
+    private static final int DAMAGE_UPGRADE = 3;
+
+
+    public final static String ID = AlchemistMod.makeID(FireStrike.class.getSimpleName());
+    public static final CardStrings CARD_STRINGS = CardCrawlGame.languagePack.getCardStrings(ID);
+    public static final String IMG_PATH = AlchemistMod.makeCardPath(FireStrike.class.getSimpleName() + ".png");
+
+    public FireStrike() {
+        super(ID, CARD_STRINGS.NAME, IMG_PATH, COST, CARD_STRINGS.DESCRIPTION, TYPE, COLOR, RARITY, TARGET);
+        this.baseDamage = DAMAGE;
+        this.tags.add(CardTags.STRIKE);
+    }
+
+    @Override
+    public void upgrade() {
+        if (!this.upgraded) {
+            this.upgradeName();
+            this.upgradeDamage(DAMAGE_UPGRADE);
+        }
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        AbstractDungeon.actionManager.addToBottom(
+                new DamageAction(m, new DamageInfo(p, this.damage, DamageInfo.DamageType.NORMAL))
+        );
+        AbstractDungeon.actionManager.addToBottom(
+                new ApplyPowerAction(p, p, new FireElement(p, p, 1), 1)
+        );
+    }
+}
