d35f1aa4c8c4f3c621f560b4f56562aa2fbe5396
IgpayAtenlay <mscron7@gmail.com>
2025-01-23 14:37:58 -0500

    added entropy, metallic skin, and protection tonic

---
+++ src/main/java/com/cron/alchemistmod/cards/MetallicSkin.java
@@ -0,0 +1,47 @@
+package com.cron.alchemistmod.cards;
+
+import com.cron.alchemistmod.AlchemistMod;
+import com.cron.alchemistmod.characters.TheAlchemist;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.localization.CardStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.PlatedArmorPower;
+
+public class MetallicSkin extends AbstractAlchemistCard {
+    private static final CardRarity RARITY = CardRarity.UNCOMMON;
+    private static final CardTarget TARGET = CardTarget.SELF;
+    private static final CardType TYPE = CardType.SKILL;
+    public static final CardColor COLOR = TheAlchemist.Enums.COLOR_GRAY;
+
+    private static final int COST = 1;
+    private static final int BLOCK = 3;
+    private static final int BLOCK_UPGRADE = 5;
+
+    public final static String ID = AlchemistMod.makeID(MetallicSkin.class.getSimpleName());
+    public static final CardStrings CARD_STRINGS = CardCrawlGame.languagePack.getCardStrings(ID);
+    public static final String IMG_PATH = AlchemistMod.makeCardPath(MetallicSkin.class.getSimpleName() + ".png");
+
+    public MetallicSkin() {
+        super(ID, CARD_STRINGS.NAME, IMG_PATH, COST, CARD_STRINGS.DESCRIPTION, TYPE, COLOR, RARITY, TARGET);
+        this.baseBlock = BLOCK;
+        this.exhaust = true;
+    }
+
+    @Override
+    public void upgrade() {
+        if (!this.upgraded) {
+            this.upgradeName();
+            this.upgradeBlock(BLOCK_UPGRADE);
+        }
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        AbstractDungeon.actionManager.addToBottom(
+                new ApplyPowerAction(p, p, new PlatedArmorPower(p, this.block))
+        );
+    }
+}
