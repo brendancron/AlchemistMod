0a070e7d0b4b6e23c09c73e8d2e5175f414a99e0
IgpayAtenlay <mscron7@gmail.com>
2025-01-24 18:14:47 -0500

    added splash

--- src/main/java/com/cron/alchemistmod/actions/RemovePotionAndApplyToAllAction.java
+++ src/main/java/com/cron/alchemistmod/actions/RemovePotionAndApplyToAllAction.java
@@ -4,14 +4,16 @@ import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.core.Settings;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.localization.UIStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import com.megacrit.cardcrawl.potions.AbstractPotion;
 import com.megacrit.cardcrawl.potions.PotionSlot;
 
 import java.util.ArrayList;
 import java.util.ListIterator;
 
-public class RemovePotionAction extends AbstractGameAction {
+public class RemovePotionAndApplyToAllAction extends AbstractGameAction {
     private final boolean first;
     public final AbstractPlayer player;
     public final int amount;
@@ -19,7 +21,7 @@ public class RemovePotionAction extends AbstractGameAction {
     public static final String[] TEXT;
 
 
-    public RemovePotionAction(AbstractPlayer player, boolean first, int amount) {
+    public RemovePotionAndApplyToAllAction(AbstractPlayer player, boolean first, int amount) {
         this.actionType = ActionType.SPECIAL;
         this.duration = Settings.ACTION_DUR_XFAST;
         this.first = first;
@@ -27,17 +29,18 @@ public class RemovePotionAction extends AbstractGameAction {
         this.amount = amount;
     }
 
-    public RemovePotionAction(AbstractPlayer player, boolean first) {
+    public RemovePotionAndApplyToAllAction(AbstractPlayer player, boolean first) {
         this(player, first, 1);
     }
 
-    public RemovePotionAction(AbstractPlayer player) {
+    public RemovePotionAndApplyToAllAction(AbstractPlayer player) {
         this(player, false);
     }
 
     public void update() {
         if (this.duration == Settings.ACTION_DUR_XFAST) {
-            ArrayList<AbstractPotion> list = null;
+            // remove potion
+            ArrayList<AbstractPotion> list;
 
             if(first) {
                 list = getFirstPotions();
@@ -48,6 +51,23 @@ public class RemovePotionAction extends AbstractGameAction {
             if (list != null) {
                 for(AbstractPotion potion: list) {
                     this.player.removePotion(potion);
+                }
+            }
+
+            // apply to all
+            if (list != null) {
+                for (AbstractPotion potion : list) {
+                    if (potion.isThrown) {
+                        if (potion.targetRequired) {
+                            for (AbstractMonster monster : AbstractDungeon.getCurrRoom().monsters.monsters) {
+                                if (!monster.isDeadOrEscaped()) {
+                                    potion.use(monster);
+                                }
+                            }
+                        } else {
+                            potion.use(AbstractDungeon.player);
+                        }
+                    }
                 }
             }
         }
