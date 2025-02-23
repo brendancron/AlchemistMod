86860fbdc4608f2555b984df701cad17aad50807
IgpayAtenlay <mscron7@gmail.com>
2025-02-23 18:59:18 -0500

    changed starting deck

--- src/main/java/com/cron/alchemistmod/cards/deprecated/ElementalBurst.java
+++ src/main/java/com/cron/alchemistmod/cards/deprecated/ElementalBurst.java
@@ -1,55 +1,50 @@
-package com.cron.alchemistmod.cards.alchemist;
+package com.cron.alchemistmod.cards.deprecated;
 
 import com.cron.alchemistmod.AlchemistMod;
 import com.cron.alchemistmod.cards.AbstractAlchemistCard;
 import com.cron.alchemistmod.characters.TheAlchemist;
-import com.cron.alchemistmod.powers.FireElement;
+import com.cron.alchemistmod.powers.AbstractElement;
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
-import com.megacrit.cardcrawl.actions.common.DamageAction;
-import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.localization.CardStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 
-public class FireStrike extends AbstractAlchemistCard {
+public class ElementalBurst extends AbstractAlchemistCard {
     private static final CardRarity RARITY = CardRarity.BASIC;
-    private static final CardTarget TARGET = CardTarget.ENEMY;
-    private static final CardType TYPE = CardType.ATTACK;
+    private static final CardTarget TARGET = CardTarget.SELF;
+    private static final CardType TYPE = CardType.SKILL;
     public static final CardColor COLOR = TheAlchemist.Enums.ALCHEMIST;
 
     private static final int COST = 1;
-    private static final int DAMAGE = 4;
-    private static final int DAMAGE_UPGRADE = 3;
 
-
-    public final static String ID = AlchemistMod.makeID(FireStrike.class.getSimpleName());
+    public final static String ID = AlchemistMod.makeID(ElementalBurst.class.getSimpleName());
     public static final CardStrings CARD_STRINGS = CardCrawlGame.languagePack.getCardStrings(ID);
-    public static final String IMG_PATH = AlchemistMod.makeAlchemistCardPath(FireStrike.class.getSimpleName() + ".png");
+    public static final String IMG_PATH = AlchemistMod.makeAlchemistCardPath(ElementalBurst.class.getSimpleName() + ".png");
 
-    public FireStrike() {
+    public ElementalBurst() {
         super(ID, CARD_STRINGS.NAME, IMG_PATH, COST, CARD_STRINGS.DESCRIPTION, TYPE, COLOR, RARITY, TARGET);
-        this.baseDamage = DAMAGE;
-        this.tags.add(CardTags.STARTER_STRIKE);
-        this.tags.add(CardTags.STRIKE);
     }
 
     @Override
     public void upgrade() {
         if (!this.upgraded) {
             this.upgradeName();
-            this.upgradeDamage(DAMAGE_UPGRADE);
+            this.rawDescription = CARD_STRINGS.UPGRADE_DESCRIPTION;
+            this.initializeDescription();
         }
     }
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
         AbstractDungeon.actionManager.addToBottom(
-                new DamageAction(m, new DamageInfo(p, this.damage, DamageInfo.DamageType.NORMAL))
+                new ApplyPowerAction(p, p, AbstractElement.getRandomBasicElement(p, p, 1), 1)
         );
-        AbstractDungeon.actionManager.addToBottom(
-                new ApplyPowerAction(p, p, new FireElement(p, p, 1), 1)
-        );
+        if (upgraded) {
+            AbstractDungeon.actionManager.addToBottom(
+                    new ApplyPowerAction(p, p, AbstractElement.getRandomBasicElement(p, p, 1), 1)
+            );
+        }
     }
 }
