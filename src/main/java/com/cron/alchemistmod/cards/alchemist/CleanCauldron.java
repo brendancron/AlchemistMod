dbd4f05c4caabb27ded669a50cdced19f2af07cd
IgpayAtenlay <mscron7@gmail.com>
2025-03-03 11:07:45 -0500

    added clean cauldron

--- src/main/java/com/cron/alchemistmod/cards/alchemist/CleanCauldron.java
+++ src/main/java/com/cron/alchemistmod/cards/alchemist/CleanCauldron.java
@@ -1,12 +1,11 @@
 package com.cron.alchemistmod.cards.alchemist;
 
 import com.cron.alchemistmod.AlchemistMod;
+import com.cron.alchemistmod.actions.LoseElementAction;
 import com.cron.alchemistmod.cards.AbstractAlchemistCard;
 import com.cron.alchemistmod.characters.TheAlchemist;
 import com.cron.alchemistmod.powers.AbstractElement;
 import com.megacrit.cardcrawl.actions.common.DamageAction;
-import com.megacrit.cardcrawl.actions.common.DrawCardAction;
-import com.megacrit.cardcrawl.actions.common.GainEnergyAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
@@ -15,25 +14,23 @@ import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.localization.CardStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 
-public class InfusedStrike extends AbstractAlchemistCard {
+public class CleanCauldron extends AbstractAlchemistCard {
     private static final CardRarity RARITY = CardRarity.UNCOMMON;
     private static final CardTarget TARGET = CardTarget.ENEMY;
     private static final CardType TYPE = CardType.ATTACK;
     public static final CardColor COLOR = TheAlchemist.Enums.ALCHEMIST;
 
     private static final int COST = 1;
-    private static final int DAMAGE = 5;
-    private static final int DAMAGE_UPGRADE = 3;
+    private static final int DAMAGE = 8;
+    private static final int DAMAGE_UPGRADE = 4;
 
-
-    public final static String ID = AlchemistMod.makeID(InfusedStrike.class.getSimpleName());
+    public final static String ID = AlchemistMod.makeID(CleanCauldron.class.getSimpleName());
     public static final CardStrings CARD_STRINGS = CardCrawlGame.languagePack.getCardStrings(ID);
-    public static final String IMG_PATH = AlchemistMod.makeAlchemistCardPath(InfusedStrike.class.getSimpleName() + ".png");
+    public static final String IMG_PATH = AlchemistMod.makeAlchemistCardPath(CleanCauldron.class.getSimpleName() + ".png");
 
-    public InfusedStrike() {
+    public CleanCauldron() {
         super(ID, CARD_STRINGS.NAME, IMG_PATH, COST, CARD_STRINGS.DESCRIPTION, TYPE, COLOR, RARITY, TARGET);
         this.baseDamage = DAMAGE;
-        this.tags.add(CardTags.STRIKE);
     }
 
     @Override
@@ -41,22 +38,24 @@ public class InfusedStrike extends AbstractAlchemistCard {
         if (!this.upgraded) {
             this.upgradeName();
             this.upgradeDamage(DAMAGE_UPGRADE);
+            this.initializeDescription();
         }
     }
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
+        if (AbstractElement.hasElement()) {
+            AbstractDungeon.actionManager.addToBottom(
+                    new LoseElementAction()
+            );
+            AbstractDungeon.actionManager.addToBottom(
+                    new DamageAction(m, new DamageInfo(p, this.damage, DamageInfo.DamageType.NORMAL))
+            );
+        }
+
         AbstractDungeon.actionManager.addToBottom(
                 new DamageAction(m, new DamageInfo(p, this.damage, DamageInfo.DamageType.NORMAL))
         );
-        if (AbstractElement.hasElement()) {
-            AbstractDungeon.actionManager.addToBottom(
-                    new GainEnergyAction(1)
-            );
-            AbstractDungeon.actionManager.addToBottom(
-                    new DrawCardAction(1)
-            );
-        }
     }
 
     public void triggerOnGlowCheck() {
