c0b8a31508c20c6121bd9f5f55ce578d2f04eb7f
IgpayAtenlay <mscron7@gmail.com>
2025-03-02 21:28:47 -0500

    added rocks fall

--- src/main/java/com/cron/alchemistmod/cards/alchemist/RocksFall.java
+++ src/main/java/com/cron/alchemistmod/cards/alchemist/RocksFall.java
@@ -1,7 +1,7 @@
 package com.cron.alchemistmod.cards.alchemist;
 
 import com.cron.alchemistmod.AlchemistMod;
-import com.cron.alchemistmod.actions.ExplosionAction;
+import com.cron.alchemistmod.actions.RocksFallAction;
 import com.cron.alchemistmod.cards.AbstractAlchemistCard;
 import com.cron.alchemistmod.characters.TheAlchemist;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
@@ -10,38 +10,38 @@ import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.localization.CardStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 
-public class Explosion extends AbstractAlchemistCard {
-    private static final CardRarity RARITY = CardRarity.RARE;
-    private static final CardTarget TARGET = CardTarget.SELF;
-    private static final CardType TYPE = CardType.SKILL;
+public class RocksFall extends AbstractAlchemistCard {
+    private static final CardRarity RARITY = CardRarity.UNCOMMON;
+    private static final CardTarget TARGET = CardTarget.ALL;
+    private static final CardType TYPE = CardType.ATTACK;
     public static final CardColor COLOR = TheAlchemist.Enums.ALCHEMIST;
 
     private static final int COST = -1;
-    private static final int MAGIC = 2;
-    private static final int MAGIC_UPGRADE = 1;
+    private static final int DAMAGE = 4;
+    private static final int DAMAGE_UPGRADE = 2;
 
-    public final static String ID = AlchemistMod.makeID(Explosion.class.getSimpleName());
+
+    public final static String ID = AlchemistMod.makeID(RocksFall.class.getSimpleName());
     public static final CardStrings CARD_STRINGS = CardCrawlGame.languagePack.getCardStrings(ID);
-    public static final String IMG_PATH = AlchemistMod.makeAlchemistCardPath(Explosion.class.getSimpleName() + ".png");
+    public static final String IMG_PATH = AlchemistMod.makeAlchemistCardPath(RocksFall.class.getSimpleName() + ".png");
 
-    public Explosion() {
+    public RocksFall() {
         super(ID, CARD_STRINGS.NAME, IMG_PATH, COST, CARD_STRINGS.DESCRIPTION, TYPE, COLOR, RARITY, TARGET);
-        this.exhaust = true;
-        this.magicNumber = this.baseMagicNumber = MAGIC;
+        this.baseDamage = DAMAGE;
     }
 
     @Override
     public void upgrade() {
         if (!this.upgraded) {
             this.upgradeName();
-            this.upgradeMagicNumber(MAGIC_UPGRADE);
+            this.upgradeDamage(DAMAGE_UPGRADE);
         }
     }
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
         AbstractDungeon.actionManager.addToBottom(
-                new ExplosionAction(this.magicNumber, this.freeToPlayOnce, this.energyOnUse)
+                new RocksFallAction(this.damage, this.freeToPlayOnce, this.energyOnUse)
         );
     }
 }
