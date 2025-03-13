21ebc27c967572616c31eaa1de516d395b779081
Brendan Cron <brendancron@gmail.com>
2025-03-12 21:02:44 -0400

    potion save bug in work

--- src/main/java/com/cron/alchemistmod/potions/BottledPotion.java
+++ src/main/java/com/cron/alchemistmod/potions/BottledPotion.java
@@ -1,16 +1,34 @@
 package com.cron.alchemistmod.potions;
 
+import basemod.abstracts.CustomSavable;
+import com.cron.alchemistmod.AlchemistMod;
+import com.cron.alchemistmod.cards.alchemist.AirBurst;
+import com.cron.alchemistmod.util.Serialization.CardDeserializer;
+import com.cron.alchemistmod.util.Serialization.CardSerializer;
+import com.cron.alchemistmod.util.Serialization.SkipFieldExclusionStrategy;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.helpers.PowerTip;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import com.megacrit.cardcrawl.potions.AbstractPotion;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
-public class BottledPotion extends AbstractPotion {
-    private static final String POTION_ID = "TheAlchemist:BottledPotion";
+public class BottledPotion extends AbstractPotion implements CustomSavable {
+    public static final String POTION_ID = AlchemistMod.makeID(BottledPotion.class.getSimpleName());;
 
-    private final AbstractCard bottledCard;
+    private static final Logger LOGGER = LogManager.getLogger(BottledPotion.class);
+
+    private static Gson GSON = new GsonBuilder()
+        .registerTypeAdapter(AbstractCard.class, new CardDeserializer())
+        .registerTypeAdapter(AbstractCard.class, new CardSerializer())
+        .setExclusionStrategies(new SkipFieldExclusionStrategy("scale"))
+        .create();
+
+    private AbstractCard bottledCard;
 
     public BottledPotion(AbstractCard card) {
         super(getPotionName(card), POTION_ID, PotionRarity.PLACEHOLDER, PotionSize.BOTTLE, PotionColor.SMOKE);
@@ -70,5 +88,23 @@ public class BottledPotion extends AbstractPotion {
 
     public static String getPotionName(AbstractCard card) {
         return "Bottled " + card.name;
+    }
+
+    @Override
+    public Object onSave() {
+        LOGGER.info("Saving Bottled Potion");
+        String json = GSON.toJson(bottledCard);
+        LOGGER.info(json);
+        return json;
+    }
+
+    @Override
+    public void onLoad(Object o) {
+        if(o instanceof String) {
+            LOGGER.info("Loading Bottled Potion");
+            String json = ((String)o);
+            LOGGER.info(json);
+            bottledCard = GSON.fromJson(json, AbstractCard.class);
+        }
     }
 }
