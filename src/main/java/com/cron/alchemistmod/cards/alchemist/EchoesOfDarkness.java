871c044d0898ebb1b819e489f47295c40eccda55
Brendan Cron <brendancron@gmail.com>
2025-10-05 18:13:18 -0400

    Echoes of Darkness and Nightmare

--- src/main/java/com/cron/alchemistmod/cards/alchemist/EchoesOfDarkness.java
+++ src/main/java/com/cron/alchemistmod/cards/alchemist/EchoesOfDarkness.java
@@ -1,46 +1,61 @@
 package com.cron.alchemistmod.cards.alchemist;
 
 import com.cron.alchemistmod.AlchemistMod;
+import com.cron.alchemistmod.actions.EchoesOfDarknessAction;
 import com.cron.alchemistmod.cards.AbstractAlchemistCard;
 import com.cron.alchemistmod.characters.TheAlchemist;
-import com.cron.alchemistmod.powers.EntropyPower;
+import com.cron.alchemistmod.powers.*;
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.localization.CardStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 
-public class Entropy extends AbstractAlchemistCard {
+public class EchoesOfDarkness extends AbstractAlchemistCard {
     private static final CardRarity RARITY = CardRarity.RARE;
     private static final CardTarget TARGET = CardTarget.SELF;
-    private static final CardType TYPE = CardType.POWER;
+    private static final CardType TYPE = CardType.SKILL;
     public static final CardColor COLOR = TheAlchemist.Enums.ALCHEMIST;
 
-    private static final int COST = 4;
+    private static final int COST = 1;
+    private static final int MAGIC = 2;
 
-    public final static String ID = AlchemistMod.makeID(Entropy.class.getSimpleName());
+    public final static String ID = AlchemistMod.makeID(EchoesOfDarkness.class.getSimpleName());
     public static final CardStrings CARD_STRINGS = CardCrawlGame.languagePack.getCardStrings(ID);
-    public static final String IMG_PATH = AlchemistMod.makeAlchemistCardPath(Entropy.class.getSimpleName() + ".png");
+    public static final String IMG_PATH = AlchemistMod.makeAlchemistCardPath(EchoesOfDarkness.class.getSimpleName() + ".png");
 
-    public Entropy() {
+    public EchoesOfDarkness() {
         super(ID, CARD_STRINGS.NAME, IMG_PATH, COST, CARD_STRINGS.DESCRIPTION, TYPE, COLOR, RARITY, TARGET);
+        this.exhaust = true;
+        this.baseMagicNumber = this.magicNumber = MAGIC;
     }
 
     @Override
     public void upgrade() {
         if (!this.upgraded) {
             this.upgradeName();
-            this.isInnate = true;
-            this.rawDescription = CARD_STRINGS.UPGRADE_DESCRIPTION;
-            this.initializeDescription();
         }
     }
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
+        int stacks = this.magicNumber;
+        if (AbstractElement.hasElement(DarkElement.class)) {
+            stacks += 2;
+        }
         AbstractDungeon.actionManager.addToBottom(
-            new ApplyPowerAction(p, p, new EntropyPower(p, p, 1), 1)
+            new EchoesOfDarknessAction(p, stacks, true)
         );
+    }
+
+    @Override
+    public void triggerOnGlowCheck() {
+        if (AbstractElement.hasElement(DarkElement.class)) {
+            this.glowColor = AbstractCard.GOLD_BORDER_GLOW_COLOR.cpy();
+        } else {
+            this.glowColor = AbstractCard.BLUE_BORDER_GLOW_COLOR.cpy();
+        }
     }
 }
