0b8f0d48fee5eda1e88ae24ee7704dda387907b3
Brendan Cron <brendancron@gmail.com>
2025-10-06 09:28:07 -0400

    Some Temporary Deprecation and Elemental Burst Changes

--- src/main/java/com/cron/alchemistmod/cards/deprecated/CleanCauldron.java
+++ src/main/java/com/cron/alchemistmod/cards/deprecated/CleanCauldron.java
@@ -1,11 +1,13 @@
-package com.cron.alchemistmod.cards.alchemist;
+package com.cron.alchemistmod.cards.deprecated;
 
 import com.cron.alchemistmod.AlchemistMod;
+import com.cron.alchemistmod.actions.LoseElementAction;
 import com.cron.alchemistmod.cards.AbstractAlchemistCard;
 import com.cron.alchemistmod.characters.TheAlchemist;
+import com.cron.alchemistmod.powers.AbstractElement;
+import com.cron.alchemistmod.powers.WaterElement;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.actions.common.DamageAction;
-import com.megacrit.cardcrawl.actions.common.DrawCardAction;
-import com.megacrit.cardcrawl.actions.common.GainEnergyAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
@@ -13,24 +15,22 @@ import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.localization.CardStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
-import com.megacrit.cardcrawl.powers.PoisonPower;
 
-public class TargetNecrosis extends AbstractAlchemistCard {
+public class CleanCauldron extends AbstractAlchemistCard {
     private static final CardRarity RARITY = CardRarity.UNCOMMON;
     private static final CardTarget TARGET = CardTarget.ENEMY;
     private static final CardType TYPE = CardType.ATTACK;
     public static final CardColor COLOR = TheAlchemist.Enums.ALCHEMIST;
 
     private static final int COST = 1;
-    private static final int DAMAGE = 5;
+    private static final int DAMAGE = 8;
     private static final int DAMAGE_UPGRADE = 3;
 
-
-    public final static String ID = AlchemistMod.makeID(TargetNecrosis.class.getSimpleName());
+    public final static String ID = AlchemistMod.makeID(CleanCauldron.class.getSimpleName());
     public static final CardStrings CARD_STRINGS = CardCrawlGame.languagePack.getCardStrings(ID);
-    public static final String IMG_PATH = AlchemistMod.makeAlchemistCardPath(TargetNecrosis.class.getSimpleName() + ".png");
+    public static final String IMG_PATH = AlchemistMod.makeAlchemistCardPath(CleanCauldron.class.getSimpleName() + ".png");
 
-    public TargetNecrosis() {
+    public CleanCauldron() {
         super(ID, CARD_STRINGS.NAME, IMG_PATH, COST, CARD_STRINGS.DESCRIPTION, TYPE, COLOR, RARITY, TARGET);
         this.baseDamage = DAMAGE;
     }
@@ -40,32 +40,30 @@ public class TargetNecrosis extends AbstractAlchemistCard {
         if (!this.upgraded) {
             this.upgradeName();
             this.upgradeDamage(DAMAGE_UPGRADE);
+            this.initializeDescription();
         }
     }
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        AbstractDungeon.actionManager.addToBottom(
-                new DamageAction(m, new DamageInfo(p, this.damage, DamageInfo.DamageType.NORMAL))
-        );
-        if (m.hasPower(PoisonPower.POWER_ID)) {
+        if (AbstractElement.hasElement()) {
             AbstractDungeon.actionManager.addToBottom(
-                    new GainEnergyAction(1)
+                new LoseElementAction()
             );
             AbstractDungeon.actionManager.addToBottom(
-                    new DrawCardAction(1)
+                new DamageAction(m, new DamageInfo(p, this.damage, DamageInfo.DamageType.NORMAL))
             );
         }
+        AbstractDungeon.actionManager.addToBottom(
+            new ApplyPowerAction(p, p, new WaterElement(p, p, 1), 1)
+        );
     }
 
     public void triggerOnGlowCheck() {
-        this.glowColor = AbstractCard.BLUE_BORDER_GLOW_COLOR.cpy();
-
-        for (AbstractMonster m : AbstractDungeon.getCurrRoom().monsters.monsters) {
-            if (!m.isDeadOrEscaped() && m.hasPower(PoisonPower.POWER_ID)) {
-                this.glowColor = AbstractCard.GOLD_BORDER_GLOW_COLOR.cpy();
-                break;
-            }
+        if (AbstractElement.hasElement()) {
+            this.glowColor = AbstractCard.GOLD_BORDER_GLOW_COLOR.cpy();
+        } else {
+            this.glowColor = AbstractCard.BLUE_BORDER_GLOW_COLOR.cpy();
         }
     }
 }
